5d249ef1418752cedfa45c2a6e825cd444ad98c5
Viktoriia Struk <struk.victoria@gmail.com>
2025-04-11 21:58:56 +0300

    Add logic for factorial

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('Hoolsd');
+var form = document.querySelector('#factorial-form');
+var stepsList = document.querySelector('#factorial-steps');
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    stepsList.textContent = '';
+    var nubmerInput = document.querySelector('#number');
+    var number = Number(nubmerInput.value);
+    var factorial = 1;
+    for (var i = 1; i <= number; i++) {
+        factorial *= i;
+        var listItem = document.createElement('li');
+        listItem.textContent = "".concat(factorial);
+        stepsList.appendChild(listItem);
+    }
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hoolsd')
+const form = document.querySelector('#factorial-form') as HTMLFormElement
+const stepsList = document.querySelector('#factorial-steps') as HTMLOListElement
+
+form.addEventListener('submit', event => {
+  event.preventDefault()
+  stepsList.textContent = ''
+  const nubmerInput = document.querySelector('#number') as HTMLInputElement
+  const number: number = Number(nubmerInput.value)
+  let factorial: number = 1
+  for (let i = 1; i <= number; i++) {
+    factorial *= i
+
+    const listItem: HTMLLIElement = document.createElement('li')
+    listItem.textContent = `${factorial}`
+    stepsList.appendChild(listItem)
+  }
+})

1282db5f02aa4c2d816c6f394afe34d340411561
Viktoriia Struk <struk.victoria@gmail.com>
2025-04-11 22:00:21 +0300

    Compilate with ES6

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-var form = document.querySelector('#factorial-form');
-var stepsList = document.querySelector('#factorial-steps');
-form.addEventListener('submit', function (event) {
+const form = document.querySelector('#factorial-form');
+const stepsList = document.querySelector('#factorial-steps');
+form.addEventListener('submit', event => {
     event.preventDefault();
     stepsList.textContent = '';
-    var nubmerInput = document.querySelector('#number');
-    var number = Number(nubmerInput.value);
-    var factorial = 1;
-    for (var i = 1; i <= number; i++) {
+    const nubmerInput = document.querySelector('#number');
+    const number = Number(nubmerInput.value);
+    let factorial = 1;
+    for (let i = 1; i <= number; i++) {
         factorial *= i;
-        var listItem = document.createElement('li');
-        listItem.textContent = "".concat(factorial);
+        const listItem = document.createElement('li');
+        listItem.textContent = `${factorial}`;
         stepsList.appendChild(listItem);
     }
 });

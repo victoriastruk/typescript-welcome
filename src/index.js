7ba2b470be996da87f32eea0b84ee7ef6e57cf45
Viktoriia Struk <struk.victoria@gmail.com>
2025-04-16 14:34:13 +0300

    Functions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,82 @@
-var AccessLevel;
-(function (AccessLevel) {
-    AccessLevel["Guest"] = "\u0413\u0456\u0441\u0442\u044C";
-    AccessLevel["User"] = "\u041A\u043E\u0440\u0438\u0441\u0442\u0443\u0432\u0430\u0447";
-    AccessLevel["Moderator"] = "\u041C\u043E\u0434\u0435\u0440\u0430\u0442\u043E\u0440";
-    AccessLevel["Admin"] = "\u0410\u0434\u043C\u0456\u043D\u0456\u0441\u0442\u0440\u0430\u0442\u043E\u0440";
-})(AccessLevel || (AccessLevel = {}));
-const currentLevel = AccessLevel.Moderator;
-console.log(`Ваш рівень доступу: ${currentLevel}`);
+/*
+Задача 1
+Створіть функцію для обчислення площі прямокутника.
+Функція приймає 2 аргументи: ширина і довжина прямокутника
+*/
+function regtangleArea(width, height) {
+    return `Площа прямоктника: ${width * height} см2`;
+}
+// console.log(regtangleArea(2, 4))
+/*
+Задача 2
+Створіть фунцію для створення об'єкта користувача з вказаним ім'ям, віком та роллю
+*/
+var Role;
+(function (Role) {
+    Role["Admin"] = "Admin";
+    Role["User"] = "User";
+    Role["Guest"] = "Guest";
+})(Role || (Role = {}));
+function createUser(name, age, role = Role.Guest) {
+    return { name, age, role };
+}
+const user1 = createUser('Олександр', 30, Role.Admin);
+const user2 = createUser('Ірина', 25, Role.User);
+const user3 = createUser('Кирило', 44);
+// console.log(user1)
+// console.log(user2)
+// console.log(user3)
+/*
+
+Задача 3
+Створіть функцію для перевірки пароля на відповідність вимогам.
+Перший аргумент - пароль.
+Опціонально, додайте другий аргумент - мінімальну довжину пароля.
+Використайте у вирішенні задачі саме опціональний аргумент!
+
+*/
+function validatePassword(password, minlength) {
+    console.log(minlength);
+    if (minlength === undefined) {
+        minlength = 6;
+    }
+    return password.length >= minlength;
+}
+// console.log(validatePassword('abc'))
+// console.log(validatePassword('strongpass', 12))
+// console.log(validatePassword('securepassword', 5))
+/*
+Задача 4.
+Напишіть функцію, яка приймає довільну кількість чисел і знаходить їх середнє арифметичне.
+Використайте rest оператор
+*/
+function calculateAverage(...numbers) {
+    if (numbers.length === 0) {
+        console.log('Список чисел порожній!');
+        return 0;
+    }
+    const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    return sum / numbers.length;
+}
+function calculateOrderTotal(products) {
+    if (products.length === 0) {
+        console.log('Немає замовлень');
+        return 0;
+    }
+    let totalPrice = 0;
+    for (const product of products) {
+        if (product.price < 0) {
+            console.log(`Некоректна ціна товару "${product.name}"`);
+            continue;
+        }
+        totalPrice += product.price;
+    }
+    return totalPrice;
+}
+const products = [
+    { name: 'Футболка', price: 25 },
+    { name: 'Шорти', price: 30 },
+    { name: 'Носки', price: -5 }
+];
+const orderTotal = calculateOrderTotal(products);
+console.log(orderTotal);
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,114 @@
-enum AccessLevel {
-  Guest = 'Гість',
-  User = 'Користувач',
-  Moderator = 'Модератор',
-  Admin = 'Адміністратор'
+/*
+Задача 1
+Створіть функцію для обчислення площі прямокутника.
+Функція приймає 2 аргументи: ширина і довжина прямокутника
+*/
+function regtangleArea (width: number, height: number): string {
+  return `Площа прямоктника: ${width * height} см2`
+}
+// console.log(regtangleArea(2, 4))
+
+/*
+Задача 2
+Створіть фунцію для створення об'єкта користувача з вказаним ім'ям, віком та роллю
+*/
+enum Role {
+  Admin = 'Admin',
+  User = 'User',
+  Guest = 'Guest'
+}
+type User = {
+  name: string
+  age: number
+  role: Role
+}
+function createUser (name: string, age: number, role: Role = Role.Guest): User {
+  return { name, age, role }
+}
+const user1 = createUser('Олександр', 30, Role.Admin)
+const user2 = createUser('Ірина', 25, Role.User)
+const user3 = createUser('Кирило', 44)
+
+// console.log(user1)
+// console.log(user2)
+// console.log(user3)
+/*
+
+Задача 3
+Створіть функцію для перевірки пароля на відповідність вимогам.
+Перший аргумент - пароль.
+Опціонально, додайте другий аргумент - мінімальну довжину пароля.
+Використайте у вирішенні задачі саме опціональний аргумент!
+
+*/
+function validatePassword (password: string, minlength?: number) {
+  console.log(minlength)
+  if (minlength === undefined) {
+    minlength = 6
+  }
+  return password.length >= minlength
 }
 
-const currentLevel: AccessLevel = AccessLevel.Moderator
-console.log(`Ваш рівень доступу: ${currentLevel}`)
+// console.log(validatePassword('abc'))
+// console.log(validatePassword('strongpass', 12))
+// console.log(validatePassword('securepassword', 5))
+/*
+Задача 4.
+Напишіть функцію, яка приймає довільну кількість чисел і знаходить їх середнє арифметичне.
+Використайте rest оператор
+*/
+function calculateAverage (...numbers: Array<number>): number {
+  if (numbers.length === 0) {
+    console.log('Список чисел порожній!')
+    return 0
+  }
+  const sum = numbers.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  )
+  return sum / numbers.length
+}
+// console.log(calculateAverage(5, 10, 15, 20))
+// console.log(calculateAverage(2, 4, 6))
+
+/*
+Задача на практику
+Створіть програму для обчислення вартості замовлення в інтернет-магазині
+
+Декомпозиція
+
+1. Створіть тип даних для представлення товару з назвою та ціною.
+2. Напишіть функцію для обчислення загальної вартості замовлення, яка приймає масив товарів як аргумент та повертає загальну суму
+
+Примітка. Перевірте, що масив товарів не порожній, а ціна кожного товару більше нуля
+*/
+type Product = {
+  name: string
+  price: number
+}
+
+function calculateOrderTotal (products: Array<Product>): number {
+  if (products.length === 0) {
+    console.log('Немає замовлень')
+    return 0
+  }
+
+  let totalPrice = 0
+  for (const product of products) {
+    if (product.price < 0) {
+      console.log(`Некоректна ціна товару "${product.name}"`)
+      continue
+    }
+    totalPrice += product.price
+  }
+  return totalPrice
+}
+
+const products: Array<Product> = [
+  { name: 'Футболка', price: 25 },
+  { name: 'Шорти', price: 30 },
+  { name: 'Носки', price: -5 }
+]
+
+const orderTotal = calculateOrderTotal(products)
+console.log(orderTotal)
